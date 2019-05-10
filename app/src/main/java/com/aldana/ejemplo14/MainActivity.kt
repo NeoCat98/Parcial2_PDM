3da969f02c41b7f4cea5f491f52a8e7092fb9c89
NeoCat98 <00076017@uca.edu.sv>
2019-05-09 18:48:33 -0600

    primer commit

---
+++ app/src/main/java/com/aldana/ejemplo14/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.aldana.ejemplo14
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import com.aldana.ejemplo14.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var scoreViewModel: ScoreViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        scoreViewModel = ViewModelProviders.of(this).get(ScoreViewModel::class.java)
+
+
+        var binding: ActivityMainBinding = DataBindingUtil.setContentView(
+                   this, R.layout.activity_main)
+        binding.team = scoreViewModel
+
+        bt_team_a_2_p.setOnClickListener{
+            scoreViewModel.scoreTeamA = (scoreViewModel.scoreTeamA.toInt()+2).toString()
+            binding.team = scoreViewModel
+        }
+        bt_team_a_3_p.setOnClickListener{
+            scoreViewModel.scoreTeamA = (scoreViewModel.scoreTeamA.toInt()+3).toString()
+            binding.team = scoreViewModel
+        }
+        bt_team_b_2_p.setOnClickListener{
+            scoreViewModel.scoreTeamB = (scoreViewModel.scoreTeamB.toInt()+2).toString()
+            binding.team = scoreViewModel
+        }
+        bt_team_b_3_p.setOnClickListener {
+            scoreViewModel.scoreTeamB = (scoreViewModel.scoreTeamB.toInt() + 3).toString()
+            binding.team = scoreViewModel
+        }
+        bt_team_a_free_throw.setOnClickListener{
+            scoreViewModel.scoreTeamA = (scoreViewModel.scoreTeamA.toInt()+1).toString()
+            binding.team = scoreViewModel
+        }
+        bt_team_b_free_throw.setOnClickListener{
+            scoreViewModel.scoreTeamB = (scoreViewModel.scoreTeamB.toInt()+1).toString()
+            binding.team = scoreViewModel
+        }
+        bt_reset.setOnClickListener {
+            scoreViewModel.scoreTeamA = "0"
+            scoreViewModel.scoreTeamB = "0"
+            binding.team = scoreViewModel
+        }
+    }
+
+}
